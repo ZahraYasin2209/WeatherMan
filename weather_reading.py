2f8d92d70b097bd4100ca778d875784a8bc014a0
Zahra Yasin <zahrayasin2209@gmail.com>
2025-11-13 11:18:55 +0500

    Remove all existing code from main

--- weather_reading.py
+++
@@ -1,7 +0,0 @@
-class WeatherReading:
-    """Represents a single day's weather data."""
-    def __init__(self, date, max_temp, min_temp, mean_humidity):
-        self.date = date
-        self.max_temp = max_temp
-        self.min_temp = min_temp
-        self.mean_humidity = mean_humidity
